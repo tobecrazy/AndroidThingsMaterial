b58630fe330909db15f54dd543a6407a74c7b201
Young Liu <young.liu@sap.com>
2021-01-05 22:05:53 +0800

    refactor folder

---
+++ Camera/cameraForMac.py
@@ -0,0 +1,16 @@
+import cv2
+from time import sleep
+
+cap = cv2.VideoCapture(0)
+  
+while True:
+  ret,frame = cap.read()
+  # Our operations on the frame come here
+  gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+  # Display the resulting frame
+  cv2.imshow('frame',gray)
+  if cv2.waitKey(1) & 0xFF == ord('q'):
+    break
+  
+cap.release()
+cv2.destroyAllWindows() 
